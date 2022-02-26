e39ba85a5e9cbcf29daafcdf9d7ed06d7649bb5b
Ирина Тюлькина <i.tiulkina@scout-corp.com>
2022-02-27 00:20:57 +0300

    [add] миграции

---
+++ DBRepository/Migrations/20220224203220_DeleteAuthEmployee.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace XmlReader.Data.DBRepository.Migrations
+{
+    public partial class DeleteAuthEmployee : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AuthUsers_Employees_ThisEmployeeId",
+                table: "AuthUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AuthUsers_ThisEmployeeId",
+                table: "AuthUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ThisEmployeeId",
+                table: "AuthUsers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ThisEmployeeId",
+                table: "AuthUsers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AuthUsers_ThisEmployeeId",
+                table: "AuthUsers",
+                column: "ThisEmployeeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AuthUsers_Employees_ThisEmployeeId",
+                table: "AuthUsers",
+                column: "ThisEmployeeId",
+                principalTable: "Employees",
+                principalColumn: "Id");
+        }
+    }
+}
---
+++ DBRepository/Migrations/20220224204059_addauth.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace XmlReader.Data.DBRepository.Migrations
+{
+    public partial class addauth : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AuthUserId",
+                table: "Employees",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_AuthUserId",
+                table: "Employees",
+                column: "AuthUserId",
+                unique: true,
+                filter: "[AuthUserId] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_AuthUsers_AuthUserId",
+                table: "Employees",
+                column: "AuthUserId",
+                principalTable: "AuthUsers",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_AuthUsers_AuthUserId",
+                table: "Employees");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Employees_AuthUserId",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "AuthUserId",
+                table: "Employees");
+        }
+    }
+}
