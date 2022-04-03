4e463cd89b5fed52866e7c2d357563559caa5aad
Ирина Тюлькина <i.tiulkina@scout-corp.com>
2022-04-03 18:25:47 +0300

    [fix] фронт

--- XmlReaderEmpWeb/Program.cs
+++ XmlReaderEmpWeb/Program.cs
@@ -4,6 +4,7 @@ using XmlReader.BLL.Interfaces;
 using XmlReader.BLL.Services;
 using DBRepository.Factories;
 using XmlReader.BLL.Service.Interfaces;
+using XmlReader.BLL.Service.Services;
 using XmlReader.BLL.Services.FolderServices;
 using XmlReader.Data.DBRepository.Factories;
 
@@ -11,34 +12,28 @@ var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
-//builder.Services.AddControllersWithViews();
-//builder.Services.AddEndpointsApiExplorer();
-
 builder.Services.AddControllers();
-//builder.Services.AddSwaggerGen();
-
+// Data
 builder.Services.AddDbContext<RepositoryContext>(options => options.UseSqlServer(connectionString));
 builder.Services.AddScoped<IRepositoryContextFactory>(op => new SqlRepositoryContextFactory(connectionString));
-//builder.Services.AddScoped<IEmployeeBaseService, EmployeeBaseService>();
-//builder.Services.AddScoped<IFolderBaseService, FolderBaseService>();
-//builder.Services.AddScoped<IWorkBaseService, WorkBaseService>();
 
+// Services
+builder.Services.AddScoped<IComputeObjectXmlService, ComputeObjectXmlService>();
 
+//Controllers
 builder.Services.AddMvc(options => options.EnableEndpointRouting = false);
-//builder.Services.AddMvc()
-//            .AddApplicationPart(typeof(XmlReader.WEB.Controllers.EmployeeControllers.EmployeeBaseController).Assembly)
-//            .AddApplicationPart(typeof(XmlReader.WEB.Controllers.FolderControllers.FolderBaseController).Assembly)
-//            .AddApplicationPart(typeof(XmlReader.WEB.Controllers.WorkControllers.WorkBaseController).Assembly)
-//            .AddControllersAsServices();
+builder.Services.AddMvc()
+            .AddApplicationPart(typeof(XmlReader.WEB.Controllers.ComputeObjectController).Assembly)
+            .AddControllersAsServices();
 
 var app = builder.Build();
 
-using (var serviceScope = app.Services.CreateScope())
-{
-    var services = serviceScope.ServiceProvider;
-    var factory = services.GetRequiredService<RepositoryContext>();
-    factory.Database.Migrate();
-}
+//using (var serviceScope = app.Services.CreateScope())
+//{
+//    var services = serviceScope.ServiceProvider;
+//    var factory = services.GetRequiredService<RepositoryContext>();
+//    factory.Database.Migrate();
+//}
 
 // Configure the HTTP request pipeline.
 app.UseMvc(routes =>
@@ -52,12 +47,7 @@ if (app.Environment.IsDevelopment())
 {
     // то выводим информацию об ошибке, при наличии ошибки
     app.UseDeveloperExceptionPage();
-    //app.UseSwagger();
-    //app.UseSwaggerUI(options =>
-    //{
-    //    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-    //    options.RoutePrefix = string.Empty;
-    //});
+
 }
 if (!app.Environment.IsDevelopment())
 {
--- XmlReaderEmpWeb/wwwroot/js/compute-file.js
+++ XmlReaderEmpWeb/wwwroot/js/compute-file.js
@@ -1,10 +1,21 @@
-function computeObject(){
-    var files = document.getElementsById('files');
-    var lable = document.getElementById('count-object');
-    for (i = 0; i < files.lenght; i++){
-        var request = new XMLHttpRequest();
-        request.open('GET', url);
-        request.responseType = 'text';
-        files[i];
-    }
+function computeObject() {
+    var url = '../api/ComputeObjectController/GetFolderInfoXml';
+    var file = document.getElementById('file');
+    var bet = document.getElementById('bet');
+    var label = document.getElementById('count-object');
+    fetch(url,
+        {
+            method: 'POST',
+            headers: { "Accept": "application/json" , 'Content-Type': 'application/json' },
+            body: JSON.stringify({ folderPath: file.value, bet: bet.value, countObject: 0, salary:0, countXmlFile: 0})
+        })
+        .then(
+            response => response.json()
+            .then(fileinfo => console.log("path: ", fileinfo.folderPath, "\nbet: ", fileinfo.bet))
+        )
+        .catch(
+            (e) => {
+            console.log('Error: ' + e.message);
+            console.log(e.response)}
+        );
 }
