029c1acefa52781005e7b820b27192df93d31a38
Ирина Тюлькина <i.tiulkina@scout-corp.com>
2022-04-03 18:24:51 +0300

    [add] контроллер для вычисления зп

--- XmlReader.WEB.Controllers/ComputeObjectController.cs
+++ XmlReader.WEB.Controllers/ComputeObjectController.cs
@@ -1,12 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using XmlReader.BLL.Models.Models;
+using XmlReader.BLL.Service.Interfaces;
 
 namespace XmlReader.WEB.Controllers;
 
+[Route("api/[controller]/[action]")]
+[ApiController]
 public class ComputeObjectController : Controller
 {
-    // GET
-    public IActionResult GetCountObject()
+    private readonly IComputeObjectXmlService _computeObjectXmlService;
+
+    public ComputeObjectController(IComputeObjectXmlService empService)
     {
-        return View();
+        this._computeObjectXmlService = empService;
     }
+
+    [HttpPost]
+    public ActionResult<FolderInfo> GetFolderInfoXml([FromBody] FolderInfo folderInfo)
+    {
+        if (string.IsNullOrEmpty(folderInfo.FolderPath))
+        {
+            return folderInfo;
+        }
+        var folderComputedInfo = _computeObjectXmlService.GetFolderInfoXml(folderInfo.FolderPath, folderInfo.Bet);
+        return folderComputedInfo;
+    }
+
 }
