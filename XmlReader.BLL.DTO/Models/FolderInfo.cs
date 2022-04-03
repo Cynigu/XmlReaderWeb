ded5a75dc339bea021d65578daa3e53a514a6ce2
Ирина Тюлькина <i.tiulkina@scout-corp.com>
2022-04-03 18:24:24 +0300

    [add] модели для веба

---
+++ XmlReader.BLL.DTO/Models/FolderInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace XmlReader.BLL.Models.Models
+{
+    public class FolderInfo
+    {
+        public string? FolderPath { get; set; }
+        public float Bet { get; set; }
+        public int CountObject { get; set; }
+        public float Salary { get; set; }
+        public int CountXmlFile { get; set; }
+    }
+}
---
+++ XmlReader.BLL.DTO/Models/ImageInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlReader.BLL.Models.Models
+{
+    public class ImageInfo
+    {
+    }
+}
