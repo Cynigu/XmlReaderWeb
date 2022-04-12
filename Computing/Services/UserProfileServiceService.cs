f2c5b84c91cb370bbd399acf7cab03fcc5f9285c
Irina Tiulkina <irina.tyulkina10@mail.ru>
2022-04-13 00:51:30 +0300

    [add] интерфес и класс для взаимодействия с интерыейсом пользователя

---
+++ Computing/Services/UserProfileServiceService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XmlReader.BLL.Service.Interfaces;
+using XmlReader.Data.DBRepository.Interfaces;
+
+namespace XmlReader.BLL.Service.Services
+{
+    public class UserProfileServiceService: IUserProfileService
+    {
+    }
+}
